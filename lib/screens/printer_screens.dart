5ccdd58fe840a3bc13df055c2f26c09afbe3ee76
David Milai <davidmilai67@gmail.com>
2022-11-04 10:02:29 +0300

    pos printer test

--- lib/screens/printer_screens.dart
+++ lib/screens/printer_screens.dart
@@ -1,22 +1,23 @@
+import 'package:ThermalPrinter/screens/pos_printer_screen.dart';
 import 'package:flutter/material.dart';
-
 
 class PrinterScreens extends StatelessWidget {
   const PrinterScreens({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text("Printer List")
-    ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Printer List")),
       body: Padding(
-        padding:  EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
         child: Column(
           children: [
             ElevatedButton(
-                child:           Text("POS Printer"),
-
-                onPressed: ()=>print("Hello"))
+                child: Text("POS Printer"),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PosPrinterScreen())))
           ],
         ),
       ),
